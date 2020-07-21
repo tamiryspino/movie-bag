40febc0f4aba5b8012893efc219b52b171800ce8
Tamirys Pino <taamypino@gmail.com>
2020-07-21 22:47:46 +0200

    Structuring Flask App using Blueprint

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-#~movie-bag/app.py
-
-from flask import Flask, request, Response
+from flask import Flask
 from database.db import initialize_db
-from database.movie import Movie
+from resources.movie import movies
 
 app = Flask(__name__)
 
@@ -11,39 +9,6 @@ app.config['MONGODB_SETTINGS'] = {
 }
 
 initialize_db(app)
-
-
-@app.route('/movies')
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
-
-@app.route('/movies', methods=['POST'])
-def add_movie():
-    body = request.get_json()
-    movie = Movie(**body).save()
-    id = movie.id
-    return {'id': str(id)}, 200
-
-
-@app.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    Movie.objects.get(id=id).update(**body)
-    return '', 200
-
-
-@app.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-    Movie.objects.get(id=id).delete()
-    return '', 200
-
-
-@app.route('/movies/<id>')
-def get_movie(id):
-    movies = Movie.objects.get(id=id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
+app.register_blueprint(movies)
 
 app.run()
